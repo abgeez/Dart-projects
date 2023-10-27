741180a80bb6436504c021eb105f343de89a25dc
ABGEEZ <mrabgeez>
2023-10-27 19:09:28 +0300

    print factorial of number using for loop

--- 3, print factorial of number.dart
+++ 3, print factorial of number.dart
@@ -3,8 +3,8 @@ void main() {
   int fact = 1;
   // Iterate over the numbers from 1 to the given number, multiplying the factorial by each number in turn.
   for (int i = 1; i <= num; i++) {
-    fact *= i;
+    fact *= i; //1=1*1, 2=1*2, 6=3*2, 24=6*3...
   }
   // Print the factorial to the console.
-  print("Factorial of $num is $fact");
+  print("Factorial of $num is $fact"); //Factorial of 5 = 120;
 }
